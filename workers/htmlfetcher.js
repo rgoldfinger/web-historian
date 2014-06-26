87d7f1470cbea527d6a8cab83c9648bf21bfd57a
Hack Reactor Students <rgoldfinger@gmail.com>
2014-06-26 16:37:40 -0700

    Prepare for promises

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -3,3 +3,5 @@
 var archive = require('../helpers/archive-helpers');
 
 archive.downloadUrls();
+
+//*/1 * * * * exec /Users/student/.nvm/v0.10.26/bin/node /Users/student/Code/Roger\ Goldfinger/2014-06-web-historian/workers/htmlfetcher.js
